beec19dada8b2bf00a1ea39f5d9a81be90717c05
Денис Мехтієв <denis.mehtiev1997@gmail.com>
2018-03-14 05:59:29 +0200

    Added auth with Firebase, logged screen.

--- iNorrisJokes/ViewController.swift
+++ iNorrisJokes/ViewController.swift
@@ -7,19 +7,52 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if Auth.auth().currentUser != nil {
+            self.presentLoggedScreen()
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func createAccountButton(_ sender: Any) {
+        
+        if let email = emailField.text, let password = passwordField.text {
+            Auth.auth().createUser(withEmail: email, password: password, completion: { (user, error) in
+                if let firebaseError = error {
+                    print(firebaseError.localizedDescription)
+                    return
+                }
+                self.presentLoggedScreen()
+            })
+        }
+        
     }
-
-
+    @IBAction func loginButton(_ sender: Any) {
+        if let email = emailField.text, let password = passwordField.text {
+            Auth.auth().signIn(withEmail: email, password: password, completion: { (user, error) in
+                if let firebaseError = error {
+                    print(firebaseError.localizedDescription)
+                    return
+                }
+                self.presentLoggedScreen()
+            })
+        }
+    }
+    
+    func presentLoggedScreen() {
+        let storyboard:UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let loggedInVC:LoggedInVC = storyboard.instantiateViewController(withIdentifier: "LoggedInVC") as! LoggedInVC
+        self.present(loggedInVC, animated: true, completion: nil)
+    }
 }
 
